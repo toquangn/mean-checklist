d830c64ff218bf2dca46aecf3e413d3dff099bd6
Quang <>
2019-04-08 16:26:00 -0700

    updated server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use('/api', api); // Uses api route for REST services
 
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('ng-checklist/src'));
 }
 app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'ng-checklist', 'src', 'index.html'));
 });
 
 app.listen( PORT,function(){
